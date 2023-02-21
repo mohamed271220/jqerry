7ed69bc7cd78adaa4a30f298c8c68863888b2464
mohamed271220 <mohamedaprahym@gmail.com>
2023-02-21 13:44:05 +0200

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+// $ === querrysle..all
+$("h1").addClass("big-title margin-50")
+$("h1").hasClass("big-title margin-50")
+$('h1').text('yoooooooo')
+$('button').text("do not click")
+$('button').html("<em>hello</em>")
+$('img').attr("src")
+$('a').attr("href","www.facebook.com")
+//==innerHTML
+//.removeClass()
+
